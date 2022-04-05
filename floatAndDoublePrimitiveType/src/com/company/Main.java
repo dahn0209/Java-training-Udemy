d6ef38ba0f7167d63c72db6f923de1921450ab85
dahn0209 <dahn0209@gmail.com>
2022-04-05 04:40:36 -0400

    changes float

--- floatAndDoublePrimitiveType/src/com/company/Main.java
+++ floatAndDoublePrimitiveType/src/com/company/Main.java
@@ -4,6 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int num=2;
+        float myMinFloatValue=Float.MIN_VALUE;
+        float myMaxFloatValue=Float.MAX_VALUE;
+        System.out.println("myMinFloatValue=>"+myMinFloatValue);
+        System.out.println("myMaxFloatValue=>"+myMaxFloatValue);
+
+        double myMinDoubleValue=Double.MIN_VALUE;
+        double myMaxDoubleValue=Double.MAX_VALUE;
+        System.out.println("myMinDoubleValue=>"+myMinDoubleValue);
+        System.out.println("myMaxDoubleValue=>"+myMaxDoubleValue);
+
+        int myIntValue=5;
+        float myFloatValue=(float)5.25;///this cast is treating a double as a float
+        double myDoubleValue=5.25;
+
     }
 }
