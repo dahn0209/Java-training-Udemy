e09386922b726f194a1c7fbcbcfaf84c5d582f8b
dahn0209 <dahn0209@gmail.com>
2022-04-06 01:34:39 -0400

    floatAndDoublePrimitiveType

--- floatAndDoublePrimitiveType/src/com/company/Main.java
+++ floatAndDoublePrimitiveType/src/com/company/Main.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.sql.SQLOutput;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -14,9 +16,30 @@ public class Main {
         System.out.println("myMinDoubleValue=>"+myMinDoubleValue);
         System.out.println("myMaxDoubleValue=>"+myMaxDoubleValue);
 
-        int myIntValue=5;
-        float myFloatValue=(float)5.25;///this cast is treating a double as a float
-        double myDoubleValue=5.25;
+//        int myIntValue=5;
+//        float myFloatValue=(float)5.25;///this cast is treating a double as a float
+//        double myDoubleValue=5.25;
+
+        int myIntValue=5/3;
+        float myFloatValue=5f/3f;
+        double myDoubleValue=5d/3f;
+        double myDoubleValueSecond=5.00/3.00;//////double precision type is recommended because this is faster  ////USE DOUBLE
+        System.out.println("myIntValue=>"+myIntValue);      //myIntValue=>2   int doesn't handle remainder
+        System.out.println("myFloatValue=>"+myFloatValue);///myFloatValue=>1.6666666
+        System.out.println("myDoubleValue=>"+myDoubleValue);///myDoubleValue=>1.6666666666666667
+        System.out.println("myDoubleValueSecond=>"+myDoubleValueSecond); //myDoubleValueSecond=>1.6666666666666667
+
+        double numberOfPounds=200d;
+        double convertedKilogram=numberOfPounds*0.45359237d;
+        System.out.println("numberOfPounds=>"+numberOfPounds);
+        System.out.println("convertedKilogram=>"+convertedKilogram);
+
+        double pi=3.1415927d;
+        double anotherNumber=3_000_000.4_567_890d;
+        System.out.println("pi=>"+pi);
+        System.out.println("anotherNumber==>"+anotherNumber);
+
+
 
     }
 }
