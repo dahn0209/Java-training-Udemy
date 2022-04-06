63f9b63ad5eb12697be7f7835be0764816e5b3dc
dahn0209 <dahn0209@gmail.com>
2022-04-06 02:31:28 -0400

    changes

--- CharAndBoolean/src/com/company/Main.java
+++ CharAndBoolean/src/com/company/Main.java
@@ -4,5 +4,21 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        char myChar='D';   ///char can only store a single character;
+        char myUnicodeChar='\u0044';  ///this is a unicode convention
+        System.out.println("myChar=>"+myChar);  //myChar=>D
+        System.out.println("myUnicodeChar=>"+myUnicodeChar); //myUnicodeChar=>D
+        char myCopyrightChar='\u00A9';
+        System.out.println("myCopyrightChar=>"+myCopyrightChar);//myCopyrightChar=>©
+
+        /////boolean/////
+        boolean myTrueBooleanValue=true;
+        boolean myFalseBooleanValue=false;
+
+        boolean isCustomerOverTwentyOne=true;   //best way to name boolean///
+
+
+
+
     }
 }
