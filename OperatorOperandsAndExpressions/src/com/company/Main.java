2e72eeb39ebe6630ad3ec6ac6c1346fecae95ad2
dahn0209 <dahn0209@gmail.com>
2022-04-10 01:40:12 -0400

    Operator

--- OperatorOperandsAndExpressions/src/com/company/Main.java
+++ OperatorOperandsAndExpressions/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    int result=1+2; //1+2+3
+        System.out.println(result);
     }
 }
