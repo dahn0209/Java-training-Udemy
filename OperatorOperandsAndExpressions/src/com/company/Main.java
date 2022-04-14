3a5ea4cdd46050c0c69a69313e89933b54c9acb8
dahn0209 <dahn0209@gmail.com>
2022-04-14 02:17:58 -0400

    changes

--- OperatorOperandsAndExpressions/src/com/company/Main.java
+++ OperatorOperandsAndExpressions/src/com/company/Main.java
@@ -48,9 +48,14 @@ public class Main {
             System.out.println("And I am scared of aliens");
         }
 
-        int topScore=100;
-        if(topScore==100){
-            System.out.println("You got the high score");
+        int topScore=80;
+        if(topScore<100){
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore=81;
+        if(topScore>secondTopScore && topScore<100){
+            System.out.println("Greater than second top score and less than 100");
         }
 
 
