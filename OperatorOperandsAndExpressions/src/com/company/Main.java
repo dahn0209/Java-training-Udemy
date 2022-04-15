525763fa36315a5e80b27ca74f16deaa711c480b
dahn0209 <dahn0209@gmail.com>
2022-04-14 20:48:31 -0400

    end

--- OperatorOperandsAndExpressions/src/com/company/Main.java
+++ OperatorOperandsAndExpressions/src/com/company/Main.java
@@ -53,10 +53,41 @@ public class Main {
             System.out.println("You got the high score!");
         }
 
-        int secondTopScore=81;
-        if(topScore>secondTopScore && topScore<100){
+        int secondTopScore=95;
+        if((topScore>secondTopScore) && (topScore<100)){
             System.out.println("Greater than second top score and less than 100");
         }
+
+        if((topScore>90) || (secondTopScore<=90)){
+            System.out.println("Either or both of the conditions are true");
+        }
+
+        int newValue=50;
+        if(newValue==50){
+            System.out.println("This is true");
+        }
+        boolean isCar=false;
+        if(!isCar){
+            System.out.println("This is not supposed to happen");
+        }
+
+//        isCar=true;
+        boolean wasCar=(isCar=true)?true:false;
+        if(wasCar){
+            System.out.println("wasCar is true");
+        }
+        double myFirstValue=20.00d;
+        double mySecondValue=80.00d;
+        double myValuesTotal=(myFirstValue+mySecondValue)*100.00d;
+        System.out.println("myValuesTotal= " + myValuesTotal);
+        double theRemainder=myValuesTotal % 40.00d;
+        System.out.println("theRemainder = "+ theRemainder);
+        boolean isNoRemainder=(theRemainder==0)? true: false;
+        System.out.println("isNoRemainder= "+ isNoRemainder);
+        if(!isNoRemainder){
+            System.out.println("Got some remainder");
+        }
+
 
 
     }
